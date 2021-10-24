ee96e7365725f824852d0f119a24e544ce962081
Kevin Xu <81491403+GeometricBison@users.noreply.github.com>
2021-10-23 23:48:47 -0500

    Add files via upload

---
+++ word.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  ifstream fin("word.in");
+  ofstream fout("word.out");
+
+  int n, c;
+  int counter = 0;
+  string s;
+
+  fin >> n >> c;
+
+  for(int i = 0; i < n; i++)
+    {
+    fin >> s;
+
+    if (s.length() + counter <= c)
+        {
+
+            if (counter == 0)
+            {
+                fout << s;
+
+            }
+
+            else
+            {
+                fout << " " << s;
+
+            }
+            counter += s.length();
+        }
+
+    else if (s.length() + counter > c)
+        {
+            fout << "\n" << s;
+            counter = s.length();
+        }
+    }
+
+}
